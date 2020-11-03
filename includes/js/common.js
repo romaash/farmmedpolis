353101ced5f687eb4386fc310dc477660d281579
danylyk <web.vitaly@rambler.ru>
2020-11-03 18:25:05 +0200

    Update

--- includes/js/common.js
+++ includes/js/common.js
@@ -608,7 +608,23 @@ $(".main-menu .bg").click(function(e){
   addScrollbar($("body"));
 });
 
+
+
 function initMap() {
+  google.maps.Map.prototype.setCenterWithOffset= function(latlng, offsetX, offsetY) {
+    var map = this;
+    var ov = new google.maps.OverlayView();
+    ov.onAdd = function() {
+      var proj = this.getProjection();
+      var aPoint = proj.fromLatLngToContainerPixel(latlng);
+      aPoint.x = aPoint.x+offsetX;
+      aPoint.y = aPoint.y+offsetY;
+      map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
+    }; 
+    ov.draw = function() {}; 
+    ov.setMap(this); 
+  };
+
   $("*[data-map]").each(function(i){
     var data = JSON.parse($(this).attr("data-map"));
     $(this).attr("data-map-index", i+"");
@@ -621,6 +637,8 @@ function initMap() {
       disableDefaultUI: true,
       styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
     });
+
+    map.setCenterWithOffset(new google.maps.LatLng(data.point[0], data.point[1]), 0, -120);
 
     if (data.route != undefined) {
       var line = [];
